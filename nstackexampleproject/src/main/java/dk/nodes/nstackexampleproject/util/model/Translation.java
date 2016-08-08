d9c0d299eef7c0ac1bb07af24407e09448f10950
Johnny Sørensen <joso@nodes.dk>
2016-08-08 15:53:22 +0200

    added translation file

---
+++ nstackexampleproject/src/main/java/dk/nodes/nstackexampleproject/util/model/Translation.java
@@ -0,0 +1,11 @@
+package dk.nodes.nstackexampleproject.util.model;
+
+/**
+ * Created by Gradle translation
+ */
+
+public class Translation {
+	public final static class defaultSection {
+		public static String ok = "Ok";
+	}
+}
