22af60acbe9e5b375185b0bf24dcd638f1e560c6
Mario Sanoguera de Lorenzo <masl@nodesagency.com>
2017-01-05 14:04:37 +0000

    [ci deploy]

--- app/src/main/java/dk/nodes/nstack/util/translation/Translate.java
+++ app/src/main/java/dk/nodes/nstack/util/translation/Translate.java
@@ -11,7 +11,7 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
 public @interface Translate {
-    
+
     enum AccessLevel { PUBLIC, PROTECTED, PACKAGE_PROTECTED, PRIVATE};
 
     String value() default "";
